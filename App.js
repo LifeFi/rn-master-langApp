160dc1bf6a9c67d50a7123448614eaa80980a5a1
topcircle <topcircle@gmail.com>
2022-12-27 14:41:05 +0900

    4.2

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import "react-native-console-time-polyfill";
 
-export default function App() {
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
+const App = () => {
+  const [y, setY] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+  const moveUp = () => {
+    const id = setInterval(() => setY((prev) => prev + 1), 1);
+    setIntervalId(id);
+  };
+
+  useEffect(() => {
+    if (y === 200) {
+      clearInterval(intervalId);
+    }
+  }, [y, intervalId]);
+
+  console.log("rendering");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Box
+        onPress={moveUp}
+        style={{
+          transform: [{ translateY: y }],
+        }}
+      ></Box>
+    </Container>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
