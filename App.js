0c8db5a80e522b1623ebc920cc945ecd19e81434
topcircle <topcircle@gmail.com>
2022-12-27 16:38:27 +0900

    4.7

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Animated, Text, View, TouchableOpacity, Easing } from "react-native";
+import {
+  Animated,
+  Text,
+  View,
+  TouchableOpacity,
+  Easing,
+  Pressable,
+} from "react-native";
 import styled from "styled-components/native";
 import "react-native-console-time-polyfill";
 
@@ -19,29 +26,52 @@ const AnimatedBox = Animated.createAnimatedComponent(Box);
 const App = () => {
   const [up, setUp] = useState(false);
 
-  const Y = useRef(new Animated.Value(0)).current;
+  const POSITION = useRef(new Animated.ValueXY({ x: 0, y: 300 })).current;
 
   const toggleUp = () => setUp((prev) => !prev);
 
   const moveUp = () => {
-    Animated.timing(Y, {
-      toValue: up ? 200 : -200,
-      useNativeDriver: true,
-      easing: Easing.cubic,
+    Animated.timing(POSITION, {
+      toValue: up ? 300 : -300,
+      useNativeDriver: false,
+      duration: 1000,
     }).start(toggleUp);
   };
 
-  Y.addListener(() => console.log(Y));
+  // Y_POSITION.addListener(() => console.log(opacityValue));
+
+  const opacityValue = POSITION.y.interpolate({
+    inputRange: [-300, -100, 100, 300],
+    outputRange: [1, 0, 0, 1],
+  });
+
+  const borderRadius = POSITION.y.interpolate({
+    inputRange: [-300, -100, 100, 300],
+    outputRange: [100, 0, 0, 100],
+  });
+
+  const rotation = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["360deg", "-360deg"],
+  });
+
+  const bgColor = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"],
+  });
 
   return (
     <Container>
-      <TouchableOpacity onPress={moveUp}>
+      <Pressable onPress={moveUp}>
         <AnimatedBox
           style={{
-            transform: [{ translateY: Y }],
+            opacity: opacityValue,
+            borderRadius: borderRadius,
+            backgroundColor: bgColor,
+            transform: [{ rotateY: rotation }, { translateY: POSITION.y }],
           }}
         />
-      </TouchableOpacity>
+      </Pressable>
     </Container>
   );
 };
