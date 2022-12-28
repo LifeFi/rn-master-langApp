56804b0747a96d32fb482cde71f9dc6c2b119f93
topcircle <topcircle@gmail.com>
2022-12-28 11:05:12 +0900

    4.13

--- App.js
+++ App.js
@@ -28,19 +28,32 @@ export default function App() {
         position.setValue(dx);
       },
       onPanResponderGrant: () => onPressIn(),
-      onPanResponderRelease: () => {
-        Animated.parallel([
-          onPressOut,
+      onPanResponderRelease: (_, { dx }) => {
+        if (dx < -320) {
+          console.log("dimiss to the left");
           Animated.spring(position, {
-            toValue: 0,
+            toValue: -500,
             useNativeDriver: true,
-          }),
-        ]).start();
+          }).start();
+        } else if (dx > 320) {
+          console.log("dimiss to the right");
+          Animated.spring(position, {
+            toValue: 500,
+            useNativeDriver: true,
+          }).start();
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
       },
     })
   ).current;
   const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(new Animated.Value(0)).current;
+  const rotation = position.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ["-15deg", "15deg"],
+    extrapolate: "clamp",
+  });
 
   const onPressIn = () =>
     Animated.spring(scale, { toValue: 0.95, useNativeDriver: true }).start();
@@ -49,13 +62,21 @@ export default function App() {
     toValue: 1,
     useNativeDriver: true,
   });
+  const goCenter = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
 
   return (
     <Container>
       <Card
         {...panResponder.panHandlers}
         style={{
-          transform: [{ scale }, { translateX: position }],
+          transform: [
+            { scale },
+            { translateX: position },
+            { rotateZ: rotation },
+          ],
         }}
       >
         <Ionicons name="pizza" color="#192a56" size={98} />
