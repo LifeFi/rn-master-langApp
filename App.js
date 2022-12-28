7a8e607105784978ff7a11892ed4b903db919b32
topcircle <topcircle@gmail.com>
2022-12-28 12:04:09 +0900

    4.16 Card

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React, { useRef } from "react";
-import { Animated, PanResponder, View, TouchableOpacity } from "react-native";
+import React, { useRef, useState } from "react";
+import { Animated, PanResponder, Text, View } from "react-native";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
+import icons from "./icons";
 
 const Container = styled.View`
   flex: 1;
@@ -21,92 +22,100 @@ const Card = styled(Animated.createAnimatedComponent(View))`
   position: absolute;
 `;
 
+const Btn = styled.TouchableOpacity`
+  margin: 0px 10px;
+`;
+
+const BtnContainer = styled.View`
+  flex-direction: row;
+  flex: 1;
+`;
+
 const CardContainer = styled.View`
   flex: 3;
   justify-content: center;
   align-items: center;
 `;
 
-const Btn = styled.TouchableOpacity`
-  margin: 0px 10px;
-`;
-
-const BtnContainer = styled.View`
-  flex: 1;
-  flex-direction: row;
-`;
-
 export default function App() {
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (_, { dx }) => {
-        position.setValue(dx);
-      },
-      onPanResponderGrant: () => onPressIn(),
-      onPanResponderRelease: (_, { dx }) => {
-        if (dx < -250) {
-          goLeft.start();
-        } else if (dx > 250) {
-          goRight.start();
-        } else {
-          Animated.parallel([onPressOut, goCenter]).start();
-        }
-      },
-    })
-  ).current;
+  // Values
   const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(new Animated.Value(0)).current;
   const rotation = position.interpolate({
     inputRange: [-250, 250],
     outputRange: ["-15deg", "15deg"],
-    extrapolate: "clamp",
   });
-
-  const onPressIn = () =>
-    Animated.spring(scale, { toValue: 0.95, useNativeDriver: true }).start();
-
   const secondScale = position.interpolate({
     inputRange: [-300, 0, 300],
     outputRange: [1, 0.7, 1],
     extrapolate: "clamp",
   });
-
+  // Animations
   const onPressOut = Animated.spring(scale, {
     toValue: 1,
+    useNativeDriver: true,
+  });
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.95,
     useNativeDriver: true,
   });
   const goCenter = Animated.spring(position, {
     toValue: 0,
     useNativeDriver: true,
   });
-
   const goLeft = Animated.spring(position, {
     toValue: -500,
     tension: 5,
     useNativeDriver: true,
+    restSpeedThreshold: 100,
+    restDisplacementThreshold: 100,
   });
-
   const goRight = Animated.spring(position, {
     toValue: 500,
     tension: 5,
     useNativeDriver: true,
+    restSpeedThreshold: 100,
+    restDisplacementThreshold: 100,
   });
-
+  // Pan Responders
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx }) => {
+        position.setValue(dx);
+      },
+      onPanResponderGrant: () => onPressIn.start(),
+      onPanResponderRelease: (_, { dx }) => {
+        if (dx < -250) {
+          goLeft.start(onDismiss);
+        } else if (dx > 250) {
+          goRight.start(onDismiss);
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
+      },
+    })
+  ).current;
+  // State
+  const [index, setIndex] = useState(0);
+  const onDismiss = () => {
+    scale.setValue(1);
+    setIndex((prev) => prev + 1);
+    position.setValue(0);
+    // Animated.timing(position, { toValue: 0, useNativeDriver: true }).start();
+  };
   const closePress = () => {
-    goLeft.start();
+    goLeft.start(onDismiss);
   };
   const checkPress = () => {
-    goRight.start();
+    goRight.start(onDismiss);
   };
-
   return (
     <Container>
       <CardContainer>
         <Card style={{ transform: [{ scale: secondScale }] }}>
-          <Ionicons name="beer" color="#192a56" size={98} />
+          <Ionicons name={icons[index + 1]} color="#192a56" size={98} />
         </Card>
-
         <Card
           {...panResponder.panHandlers}
           style={{
@@ -117,7 +126,7 @@ export default function App() {
             ],
           }}
         >
-          <Ionicons name="pizza" color="#192a56" size={98} />
+          <Ionicons name={icons[index]} color="#192a56" size={98} />
         </Card>
       </CardContainer>
       <BtnContainer>
